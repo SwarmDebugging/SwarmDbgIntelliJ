985e7997f4fba64681494b8fc8918a36cb28bf4a
Vincent Gagnon <vincentgagnon53@gmail.com>
2020-08-12 14:02:48 -0400

    added option to see nodes in graph sized by number of events

--- src/main/java/com/swarm/toolWindow/VisualizationToolWindow.java
+++ src/main/java/com/swarm/toolWindow/VisualizationToolWindow.java
@@ -5,20 +5,25 @@ import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.ActionToolbar;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.SimpleToolWindowPanel;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.jcef.JBCefApp;
 import com.intellij.ui.jcef.JBCefBrowser;
 import com.intellij.util.ui.JBUI;
+import com.swarm.models.Product;
 import com.swarm.models.Task;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
 public class VisualizationToolWindow extends SimpleToolWindowPanel implements DumbAware, TreeSelectionProvider.Handler, Disposable {
     private final Project project;
+    private boolean constantNodeSize = true;
 
     @Override
     public JComponent getContent() {
@@ -37,7 +42,7 @@ public class VisualizationToolWindow extends SimpleToolWindowPanel implements Du
 
     private void createToolBar() {
         final DefaultActionGroup group = new DefaultActionGroup();
-
+        group.add(new ToggleNodeSizeAction());
         final ActionToolbar actionToolbar = ActionManager.getInstance().createActionToolbar("swarm", group, false);
         setToolbar(JBUI.Panels.simplePanel(actionToolbar.getComponent()));
     }
@@ -51,9 +56,9 @@ public class VisualizationToolWindow extends SimpleToolWindowPanel implements Du
             Task task = (Task) treeNode;
 
             if(JBCefApp.isSupported()){
-                setContent(new JBCefBrowser("http://localhost:8080/getInvocationGraph/" + task.getId()).getComponent());
+                setContent(new JBCefBrowser("http://localhost:8080/getInvocationGraph/" + task.getId() + "/" + constantNodeSize).getComponent());
             } else {
-                BrowserUtil.browse("http://localhost:8080/getInvocationGraph/" + task.getId());
+                BrowserUtil.browse("http://localhost:8080/getInvocationGraph/" + task.getId() + "/" + constantNodeSize);
             }
         } else {
             setContent(new JBLabel("Select a task to view the visualisation"));
@@ -63,5 +68,23 @@ public class VisualizationToolWindow extends SimpleToolWindowPanel implements Du
 
     @Override
     public void dispose() {
+    }
+
+    private class ToggleNodeSizeAction extends DumbAwareAction {
+        ToggleNodeSizeAction() {
+            super("Toggle Node Sized to Number of Events");
+        }
+
+        @Override
+        public void actionPerformed(@NotNull AnActionEvent e) {
+            constantNodeSize = !constantNodeSize;
+            TreeSelectionProvider.setTreeNode(TreeSelectionProvider.getTreeNode());
+        }
+
+        @Override
+        public void update(@NotNull AnActionEvent e) {
+            super.update(e);
+            e.getPresentation().setEnabled(TreeSelectionProvider.getTreeNode() instanceof Task);
+        }
     }
 }
